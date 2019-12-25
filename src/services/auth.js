7340932fbe5397e3c020f95acefa3fee50fd8f36
andreeiruan <andreicruz.silva8765@gmail.com>
2019-12-25 11:23:31 -0300

    Cria cookie de autenticação

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,24 @@
+
 export const TOKEN_KEY = ''
-export const isAuthenticated = () => localStorage.getItem('TOKEN_KEY') !== null
-export const getToken = () => localStorage.getItem('TOKEN_KEY')
+export const isAuthenticated = () => getToken() !== undefined
+export const getToken = () => {
+  let cookie = decodeURIComponent(document.cookie)
+  cookie = cookie.split(';')
+  for(let item of cookie){
+    let c = item
+    c = c.split('=')
+    c = c[1]    
+    return c   
+  }
+}
 export const login = (token, id) => {
-  localStorage.setItem('idUser', id)
-  localStorage.setItem('TOKEN_KEY', token)
+  const date = new Date()
+  date.setTime(date.getTime() + (1*24*60*60*1000))
+  localStorage.setItem('idUser', id) 
+  document.cookie = `authToken=${token};expires=${date.toGMTString()}`
 }
 export const logout = () => {
-  localStorage.removeItem('idUser')
-  return localStorage.removeItem('TOKEN_KEY')
+  const date = new Date()
+  date.setTime(date.getTime() - (4*24*60*60*1000))  
+  return document.cookie = `authToken=; expires=${date.toGMTString()}`
 }
